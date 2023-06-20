616eba4dc62e279830feb388b65890eb62b587bf
G <c0b2202637@edu.teu.ac.jp>
2023-06-20 15:07:53 +0900

    練習3

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -3,7 +3,13 @@ import sys
 import pygame as pg
 
 
-WIDTH, HEIGHT = 1600, 800
+WIDTH, HEIGHT = 1600, 900
+delta = {
+    pg.K_UP:(0, -5),
+    pg.K_DOWN:(0, +5),
+    pg.K_LEFT:(-5, 0),
+    pg.K_RIGHT:(+5, 0)
+}
 
 
 def main():
@@ -12,6 +18,9 @@ def main():
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+
+    kk_rct = kk_img.get_rect()
+    kk_rct.center = 900,400
 
     bd_img = pg.Surface((20,20))  #練習1
     bd_img.set_colorkey((0,0,0))
@@ -30,13 +39,22 @@ def main():
             if event.type == pg.QUIT: 
                 return
 
+        key_lst = pg.key.get_pressed()
+        sum_mv = [0, 0]
+        for k, mv in delta.items():
+            if key_lst[k]:
+                sum_mv[0] += mv[0]
+                sum_mv[1] += mv[1]
+        kk_rct.move_ip(sum_mv)
+
         bd_rct.move_ip(vx, vy)
         screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img, [900, 400])
+        screen.blit(kk_img, kk_rct)
+        bd_rct.move_ip(vx, vy)
         screen.blit(bd_img, bd_rct)
         pg.display.update()
         tmr += 1
-        clock.tick(10)
+        clock.tick(50)
 
 
 
